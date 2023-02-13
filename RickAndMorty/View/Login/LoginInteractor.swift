337d882a39df314b456d488c8758cf2089dd1041
Gabriel Castro <t786832@f1rst.com.br>
2023-02-12 21:37:48 -0300

    clean

--- RickAndMorty/View/Login/LoginInteractor.swift
+++ RickAndMorty/View/Login/LoginInteractor.swift
@@ -17,20 +17,6 @@ final class LoginInteractor {
     var isNotValidUser: ((Error) -> Void)?
     
     func validateUserLogin(email: String, password: String) {
-        //        let isLoginValid = logins.contains(
-        //            where: { $0.login == loginInputed.login }
-        //        )
-        //        let isPasswordValid = logins.contains(
-        //            where: { $0.password == loginInputed.password }
-        //        )
-        //
-        //        if  isLoginValid && isPasswordValid {
-        //            self.isUserValid?()
-        //        } else {
-        //            //TODO: Implementar caso erro
-        //            self.isNotValidUser?("Error")
-        //        }
-        
         Auth.auth().signIn(
             withEmail: email,
             password: password) {
